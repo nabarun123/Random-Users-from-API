f9f15a0ac8d9a0d8e8226d500c0e2b6f4e729c7b
nabarun123 <nabarunjyotiboruah@gmail.com>
2022-06-15 22:25:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState([]);
+  const fetchUsers = async () => {
+    const data = await axios.get("https://randomuser.me/api/?results=50");
+    setUser(data.data.results);
+    console.log(data.results);
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.map((item) => (
+        <div className="card m-2 p-2 " style={{ width: 18 + "rem" }}>
+          <img src={item.picture.large} className="card-img-top" />
+          <div className="card-body">
+            <h3 className="card-title text-primary">
+              {item.name.title} {item.name.first} {item.name.last}
+            </h3>
+
+            <b>{item.phone}</b>
+            <br />
+            <em>{item.email}</em>
+            <br />
+            <b>
+              {item.location.city}, {item.location.country}
+            </b>
+            <br />
+            <br />
+            <hr />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
